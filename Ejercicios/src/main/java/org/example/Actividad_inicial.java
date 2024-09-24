d0a65534739684991b1dcec8ef1302bee893d64b
A700\1dam <p.benaventedomenec@edu.gva.es>
2024-09-24 18:40:17 +0200

    cambios

---
+++ Ejercicios/src/main/java/org/example/Actividad_inicial.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Actividad_inicial {
+
+    public void ejecutar() {
+
+        System.out.println("¿Cómo te llamas?");
+
+        Scanner entrada = new Scanner(System.in);
+        String nombre = entrada.next();
+
+        System.out.println("¿A qué curso vas?");
+        String curso = entrada.next();
+
+        System.out.println("Hola " + nombre + ", bienvenido a " + curso + ".");
+
+        System.out.println("Introduce un número:");
+        int num1 = entrada.nextInt();
+
+        System.out.println("Introduce otro número:");
+        int num2 = entrada.nextInt();
+
+        int resultado = num1 + num2;
+
+        System.out.println("El resultado de la suma es: " + resultado);
+
+    }
+
+}
