116aea934d9f3f4eed4087c1c250e802fbe4c663
A700\1dam <p.benaventedomenec@edu.gva.es>
2024-11-11 21:18:45 +0100

    ++

--- Ejercicios/src/main/java/org/example/Main.java
+++ Ejercicios/src/main/java/org/example/Main.java
@@ -44,7 +44,7 @@ public class Main {
 //        ejercicios.prueba();
 
 Vectores vectores = new Vectores();
-vectores.ejercicio5();
+vectores.ruleta();
 
 
 
--- Ejercicios/src/main/java/org/example/Vectores.java
+++ Ejercicios/src/main/java/org/example/Vectores.java
@@ -1,8 +1,8 @@
 package org.example;
 
-import java.util.Arrays;
-import java.util.Random;
-import java.util.Scanner;
+import java.time.LocalDateTime;
+import java.time.Year;
+import java.util.*;
 
 public class Vectores {
 
@@ -190,6 +190,158 @@ public class Vectores {
 
 
     }
+
+    public void navidad(){
+
+        Scanner teclado = new Scanner(System.in);
+
+        final String PALABRA = "NAVIDAD";
+
+        String palabras[] = PALABRA.split("");
+
+        System.out.println(Arrays.toString(palabras));
+
+        System.out.println("Introduce la cantidad de letras que quieres...");
+        String cantidad = teclado.nextLine();
+        String cantidades[] =  cantidad.split(" ");
+
+        String resultado = "";
+
+        System.out.println(Arrays.toString(cantidades));
+
+        if (cantidades.length==palabras.length){
+            for (int i=0;i<cantidades.length;i++){
+
+                for (int j=0; j<Integer.parseInt(cantidades[i]);j++){
+
+                    resultado = resultado + palabras[i];
+
+                }
+
+            }
+
+            System.out.println(resultado);
+        }else{
+            System.out.println("No hay 1 número por letra.");
+        }
+
+    }
+
+    public void buscar(){
+
+        String nombres[] = {"Paco", "Jose", "Ximo", "Alfonso"};
+
+        String palabra_buscar = "Pepe";
+
+        boolean existe = Arrays.asList(nombres).contains(palabra_buscar);
+
+        if (existe){
+            System.out.println(palabra_buscar +  " existe.");
+        }else{
+            System.out.println(palabra_buscar + " no existe en el vector.");
+        }
+
+        Integer numeros[] = {3,5,6,8,24,56};
+
+        int num = 6;
+
+        boolean num_existe = Arrays.asList(numeros).contains(num);
+
+        if (num_existe){
+            System.out.println(num + " existe en el array.");
+        }else{
+            System.out.println(num + " no existe en el array.");
+        }
+
+
+
+
+
+    }
+
+    public void ruleta(){
+
+        Scanner teclado = new Scanner(System.in);
+        Random aleatorio = new Random();
+
+        String colores[] = {"rojo","negro"};
+
+        Integer numeros[] = new Integer[37];
+
+        for (int i=0; i<numeros.length;i++){
+            numeros[i] = i;
+        }
+
+        String pares[] = {"par","impar"};
+
+        System.out.println("*****BIENVENIDO A LA RULETA******");
+
+        System.out.println("Introduce un número (0-36): ");
+        int numero = teclado.nextInt();
+
+        boolean numero_encontrado = Arrays.asList(numeros).contains(numero);
+
+        String color = "";
+        String par = "";
+
+        if (!numero_encontrado){
+            System.out.println("ERROR. El número no es válido");
+            return;
+        }else{
+            if (numero!=0){
+                System.out.println("Inserta un color...");
+                color = teclado.next();
+                boolean color_encontrado = Arrays.asList(colores).contains(color);
+
+                if (!color_encontrado){
+                    System.out.println("ERROR. El color no es válido");
+                    return;
+                }else{
+                    System.out.println("Inserta par/impar: ");
+                    par = teclado.next();
+                    boolean par_encontrado = Arrays.asList(pares).contains(par);
+
+                    if(!par_encontrado){
+                        System.out.println("ERROR. La opción par/impar no es válida.");
+                        return;
+                    }
+                }
+
+            }
+
+        }
+
+        String sorteo_color = colores[aleatorio.nextInt(colores.length)];
+        int sorteo_numero = numeros[aleatorio.nextInt(numeros.length)];
+        String sorteo_par = "";
+
+        if (sorteo_numero%2==0){
+            sorteo_par = "par";
+        }else{
+            sorteo_par = "impar";
+        }
+
+        System.out.println("Ha tocado " + sorteo_numero + " " + sorteo_color + " " + sorteo_par);
+
+        if (sorteo_numero==numero && sorteo_color.equals(color) && sorteo_par.equals(par)){
+            System.out.println("HAS GANADO!!!");
+        } else if (numero!=0 && sorteo_color.equals(color)) {
+            System.out.println("Has acertado el color.");
+        } else if (numero!=0 && sorteo_par.equals(par)) {
+            System.out.println("Has acertado la opción " + par);
+        } else if (numero!= 0 && numero==sorteo_numero) {
+            System.out.println("Has acertado el número");
+        } else if (numero==0 && sorteo_numero==0) {
+            System.out.println("HAS GANADO y los demás usuarios pierden.");
+        }else{
+            System.out.println("Has perdido.");
+        }
+
+
+    }
+
+
+
 
 
 
